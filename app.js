c5db7b189f21f28a3ee9a2132cd59636dd82db82
Worth Baker <wbaker@greenmtd.com>
2016-04-13 18:05:55 -0600

    added form support

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 'use strict';
 
 var getsoundcloud = require('./modules/getsoundcloud');
+var path    = require("path");
+var bodyParser = require('body-parser')
 var express = require('express')
 var app = express()
 
-app.get('/soundcloud', function(req, res){
-  var url = req.query.id
-  console.log(url);
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+app.post('/', function(req, res){
+  var url = req.body.scURL
+  console.log('hello');
   getsoundcloud(url, function(err, trackID) {
     if (err) {
-      console.log('ERRRRRROR');
       res.status(500);
-      // res.json({ 'error': 'err' });
       res.send('<h1>An internal error occured.</h1>')
       res.end();
     } else {
-      console.log('TRACK ID' + trackID);
+      res.status(200);
       res.send('<p>URL: ' + url + '\n</p><p>SoundCloud Track ID: ' + trackID + '</p>')
       res.end();
     }
